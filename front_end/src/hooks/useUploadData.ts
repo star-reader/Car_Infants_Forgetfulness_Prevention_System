302f61ab3b859adf22a38540e2d4a40a560c87ba
star-reader <75555433+star-reader@users.noreply.github.com>
2023-10-24 12:54:44 +0800

    update weather api
    根据天气API，完成了对城市温度的获取

--- front_end/src/hooks/useUploadData.ts
+++ front_end/src/hooks/useUploadData.ts
@@ -1,15 +1,16 @@
-import mqtt from 'mqtt'
 import { getMqttServer, setMqttServer } from './useMqttServer'
+import useWeather from './useWeather'
 
-export default () => {
+export default async () => {
     let client = getMqttServer()
     if (!client) return setMqttServer()
     //时间使用时间戳
     //天气API还在查找，目前有些需要token等授权，所以暂时没弄好，这个以后找到后就可以随时弄
     // 天气对应的城市，先默认是广州市，后续根据接口内容进行调整
+    const temperature = await useWeather()
     const data = {
         'time': Date.now(),
-        'temperature': 30
+        temperature
     }
     client.publish('api', JSON.stringify(data))
 }
